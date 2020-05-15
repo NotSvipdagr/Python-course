aa24638e12fc17e0d3d6c1a5480c00f36c6f75a0
NotSvipdagr <royonjeanchristophe@gmail.com>
2020-05-15 16:06:35 +0200

    commit

---
+++ mystuff/ex11.py
@@ -0,0 +1,15 @@
+print ("How old are you?"),
+age = input()
+print("How tall are you?"),
+height = input()
+print ("How much do you weigh?"),
+weight = input()
+
+print("So, you're %r old, %r tall and %r heavy." % (
+    age, height, weight))
+
+
+#Drill 3
+print("What's your name dude?"),
+name = input()
+print("Let me be your friend %s" % (name))
