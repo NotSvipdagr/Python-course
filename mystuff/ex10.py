022d5ebc7276dc41193844505697bbe2c872cc1f
NotSvipdagr <royonjeanchristophe@gmail.com>
2020-05-15 15:47:39 +0200

    comit

---
+++ mystuff/ex10.py
@@ -0,0 +1,29 @@
+tabby_cat = "\t I'm tabbed in."
+persian_cat = "I'm split\non a lie."
+backslash_cat = "I'm \\ a \\ cat."
+#drill 3
+bob_cat = """\b \\\\ That's how \'you\' do it 
+\tLike a boss"""
+#drill 4 
+gros_cat = '"that\'s"'
+kitty_cat = 'Let me try "this" : %s' % gros_cat
+
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
+print(bob_cat)
+print(kitty_cat)
+
+#Try me : 
+#while True:
+#    for i in ["/","-","|","\\",""] : 
+#       print ("%s\r" % i,)
