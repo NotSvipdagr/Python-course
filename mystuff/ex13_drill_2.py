1723ea9549bfa198d9a21063780776f77db14ea0
NotSvipdagr <royonjeanchristophe@gmail.com>
2020-05-15 19:33:11 +0200

    commit

---
+++ mystuff/ex13_drill_2.py
@@ -0,0 +1,9 @@
+from sys import argv
+
+menu, entrée, plat, dessert, café = argv
+
+print("C'est le menu:", menu)
+print("on commence par", entrée)
+print("ensuite", plat)
+print("pour les gourmand:", dessert)
+
